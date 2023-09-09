6d93ddf0046bfd3f906c7b80c4059377e248021e
Destcable <artmobpavlov21@gmail.com>
2023-09-09 14:32:08 +0300

    [UPD]

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Header from './components/header/header';
-import './components/header/header.css';
 import Card from "./components/card/card";
+import './components/header/header.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
