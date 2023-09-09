fcdedf1e7e89424c6694f4fa4710b31a674608b0
Destcable <artmobpavlov21@gmail.com>
2023-09-09 18:24:33 +0300

    [UPD]

---
+++ src/components/tringle/tringle.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import tringle from './img/tringle.png';
+
+const Tringle = () => {
+    return (
+            <image src={tringle} alt="tri"/>
+    )
+}
+
+export default Tringle;
