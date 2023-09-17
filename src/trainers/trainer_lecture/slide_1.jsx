a18753b051121822c1ebf2a232989c41e21dbbfe
Destcable <artmobpavlov21@gmail.com>
2023-09-17 15:57:51 +0300

    [UPD]

---
+++ src/trainers/trainer_lecture/slide_1.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import VidOborudovania from "../../pages/traning/trainings/img/training_lecture/01 Виды оборудования.jpg"
+
+const TrainerLecture = () => { 
+    
+    return( 
+        <>
+            <div>
+                <img src={VidOborudovania} />
+                Назначение, виды и классификация металлорежущего оборудования
+            </div>
+        </>
+    )
+};
+
+export default TrainerLecture;
