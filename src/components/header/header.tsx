c67267f39ebaa2e6c147de64b297e8cd75373d3f
Destcable <artmobpavlov21@gmail.com>
2023-09-21 16:13:22 +0300

    [UPD]

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,35 +1,35 @@
-import React, {CSSProperties} from 'react';
+import React, { CSSProperties } from 'react';
 import Logo from '../logo/logo';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../styles/main.css';
 
-interface HeaderProps { 
+interface HeaderProps {
     title: string;
 }
 
 const Header = (HeaderObj: HeaderProps) => {
 
-    const styleHeader: CSSProperties = { 
+    const styleHeader: CSSProperties = {
         alignItems: "center",
         width: "100%",
         height: "104px",
         backgroundColor: "#3B5571"
     }
 
-    const styleLogo: CSSProperties = { 
-        position: "absolute", 
-        zIndex: "3", 
+    const styleLogo: CSSProperties = {
+        position: "absolute",
+        zIndex: "3",
         marginTop: "20px"
     }
 
-    const styleButton: CSSProperties = { 
+    const styleButton: CSSProperties = {
         borderRadius: "15px",
-        fontWeight: "bold", 
-        fontSize: "14px", 
-        padding:"14px 32px 25px"
+        fontWeight: "bold",
+        fontSize: "14px",
+        padding: "14px 32px 25px"
     }
 
-    const styleHeaderSecond: CSSProperties = { 
+    const styleHeaderSecond: CSSProperties = {
         position: "absolute",
         width: "100%",
         top: "54px",
@@ -37,29 +37,38 @@ const Header = (HeaderObj: HeaderProps) => {
         backgroundColor: "#43698F",
     }
 
+    const styleSubtitle: CSSProperties = {
+        position: "absolute",
+        color: "#A2BEDD",
+        fontWeight: "bold",
+        fontSize: "20px",
+        zIndex: 3,
+        paddingTop: "0px",
+    }
+
     return (
         <header className="d-flex flex-column" style={styleHeader}>
-        <div className="container d-flex flex-row">
-            <div style={styleLogo}>
-                <Logo />
-            </div>
-            <div className="d-flex flex-column ml-90 w-100">
-                <div className="d-flex flex-wrap w-100 justify-content-between">
-                    <div className="pt-2">
-                        <h2 className="text-white">{HeaderObj.title}</h2></div>
-                    <div className="d-flex align-items-center mt-10">
-                        <a href='#' className="btn-contacts text-white" style={styleButton}>Информация о контенте</a>
-                        <a href='#' className="btn-words text-white" style={styleButton}>Ключевые слова</a>
-                    </div>
-                    
+            <div className="container d-flex flex-row">
+                <div style={styleLogo}>
+                    <Logo />
                 </div>
-                <div>
-                    <h3 className="subtitle_header">Выбор оборудования, инструмента и технологической оснастки</h3>
-                 </div>
+                <div className="d-flex flex-column ml-90 w-100">
+                    <div className="d-flex flex-wrap w-100 justify-content-between">
+                        <div className="pt-2">
+                            <h2 className="text-white">{HeaderObj.title}</h2></div>
+                        <div className="d-flex align-items-center mt-10">
+                            <a href='#' className="btn-contacts text-white" style={styleButton}>Информация о контенте</a>
+                            <a href='#' className="btn-words text-white" style={styleButton}>Ключевые слова</a>
+                        </div>
+
+                    </div>
+                    <div>
+                        <h3 style={styleSubtitle}>Выбор оборудования, инструмента и технологической оснастки</h3>
+                    </div>
+                </div>
             </div>
-        </div>
-            <div style={styleHeaderSecond}></div>      
-    </header>
+            <div style={styleHeaderSecond}></div>
+        </header>
     )
 }
 
