5e33a087487cdfcac1dcf3a1cc52c17b92a39df5
Destcable <artmobpavlov21@gmail.com>
2023-09-23 00:36:09 +0300

    [UPD]

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,6 +1,7 @@
 import React, { CSSProperties } from "react";
 import ModalBootstrap from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 interface ModalProps {
     active: boolean,
@@ -8,23 +9,36 @@ interface ModalProps {
 }
 
 const Modal = (ModalObj: ModalProps) => {
+
     const handleClose = () => ModalObj.setActive(false);
+
+    const styleModal: CSSProperties = { 
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+        width: "100vw",
+        backgroundColor: "rgba(0, 0, 0, 0.2)",
+        position: "fixed",
+        top: 0,
+        left: 0,
+        zIndex: 5,
+    };
+
+    const styleModalContext: CSSProperties = { 
+        padding: "20px",
+        backgroundColor: "white",
+        height: "200px",
+        width: "400px",
+    }
+
     return (
         <>
-            <ModalBootstrap show={ModalObj.active} onHide={handleClose}>
-                <ModalBootstrap.Header closeButton>
-                    <ModalBootstrap.Title>Modal heading</ModalBootstrap.Title>
-                </ModalBootstrap.Header>
-                <ModalBootstrap.Body>Woohoo, you are reading this text in a modal!</ModalBootstrap.Body>
-                <ModalBootstrap.Footer>
-                    <Button variant="secondary">
-                        Close
-                    </Button>
-                    <Button variant="primary">
-                        Save Changes
-                    </Button>
-                </ModalBootstrap.Footer>
-            </ModalBootstrap>
+        <div style={styleModal}>
+            <div style={styleModalContext}>
+
+            </div>
+        </div>
         </>
     )
 };
