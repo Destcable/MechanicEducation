1d357ff18df3cab9c182c05d050657b63f242927
Destcable <artmobpavlov21@gmail.com>
2023-09-29 21:54:20 +0300

    [UPD]

--- src/pages/lectureMain/LectureMainPage.tsx
+++ src/pages/lectureMain/LectureMainPage.tsx
@@ -3,10 +3,6 @@ import { useNavigate } from "react-router-dom";
 import Header from '../../components/header/header';
 import Tringle from '../../components/tringle/tringle';
 import HomeButton from "../../components/homeButton/homeButton";
-import VidOborudovania from "../traning/trainings/img/training_lecture/01 Виды оборудования.jpg";
-import ViborStankov from "../traning/trainings/img/training_lecture/02 Выбор станков.jpg";
-import VidiInstruments from "../traning/trainings/img/training_lecture/03 Виды инструментов.jpg";
-import RezInstrument from "../traning/trainings/img/training_lecture/04 Основы выбора режущего инструмента.jpg";
 
 function LectureMainPage () {
     const navigate = useNavigate();
@@ -32,19 +28,22 @@ function LectureMainPage () {
                             <div className="container-exercise">
                                 <div className="d-flex justify-center align-center" style={{ flexWrap: "wrap"}}>
                                     <div style={{ display: "flex", alignItems: "center" }} onClick={() => navigate('/lecture_1')}>
-                                        <img src={VidOborudovania} width="300px" />
+                                        <img src="https://firebasestorage.googleapis.com/v0/b/mechaniceducation-bebac.appspot.com/o/images%2F01%20%D0%92%D0%B8%D0%B4%D1%8B%20%D0%BE%D0%B1%D0%BE%D1%80%D1%83%D0%B4%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F.jpg?alt=media&token=9d9fc362-b324-42c6-b920-05cde91787e0&_gl=1*1q3f5rk*_ga*MTkzMTEyODI0NS4xNjk1ODIyNDgx*_ga_CW55HF8NVT*MTY5NjAxMzM0Ny4xMS4xLjE2OTYwMTM1ODIuNTEuMC4w" 
+                                            width="300px" />
                                         Назначение, виды и классификация металлорежущего оборудования
                                     </div>
                                     <div style={{ display: "flex", alignItems: "center" }} onClick={() => navigate('/lecture_2')}>
-                                        <img src={ViborStankov} width="270px" />
+                                        <img src="https://firebasestorage.googleapis.com/v0/b/mechaniceducation-bebac.appspot.com/o/images%2F02%20%D0%92%D1%8B%D0%B1%D0%BE%D1%80%20%D1%81%D1%82%D0%B0%D0%BD%D0%BA%D0%BE%D0%B2.jpg?alt=media&token=6fd77aa0-0432-4547-a3a6-89f015a586f7&_gl=1*1dwp7qx*_ga*MTkzMTEyODI0NS4xNjk1ODIyNDgx*_ga_CW55HF8NVT*MTY5NjAxMzM0Ny4xMS4xLjE2OTYwMTM1OTkuMzQuMC4w" 
+                                            width="270px" />
                                         Выбор металлорежущего оборудования для реализации технологического процесса
                                     </div>
                                     <div style={{ display: "flex", alignItems: "center" }} onClick={() => navigate('/lecture_3')}>
-                                        <img src={VidiInstruments} width="300px" />
+                                        <img src="https://firebasestorage.googleapis.com/v0/b/mechaniceducation-bebac.appspot.com/o/images%2F03%20%D0%92%D0%B8%D0%B4%D1%8B%20%D0%B8%D0%BD%D1%81%D1%82%D1%80%D1%83%D0%BC%D0%B5%D0%BD%D1%82%D0%BE%D0%B2.jpg?alt=media&token=f95e0c7d-bb26-466e-a184-56bb36ec73d3&_gl=1*1xx7n1t*_ga*MTkzMTEyODI0NS4xNjk1ODIyNDgx*_ga_CW55HF8NVT*MTY5NjAxMzM0Ny4xMS4xLjE2OTYwMTM2MTguMTUuMC4w" 
+                                            width="300px" />
                                         Режущий инструмент: типы, виды исполнения и материалы режущей части инструмента
                                     </div>
                                     <div style={{ display: "flex", alignItems: "center" }} onClick={() => navigate('/lecture_4')}>
-                                        <img src={RezInstrument} width="300px" />
+                                        <img src="https://firebasestorage.googleapis.com/v0/b/mechaniceducation-bebac.appspot.com/o/images%2F04%20%D0%9E%D1%81%D0%BD%D0%BE%D0%B2%D1%8B%20%D0%B2%D1%8B%D0%B1%D0%BE%D1%80%D0%B0%20%D1%80%D0%B5%D0%B6%D1%83%D1%89%D0%B5%D0%B3%D0%BE%20%D0%B8%D0%BD%D1%81%D1%82%D1%80%D1%83%D0%BC%D0%B5%D0%BD%D1%82%D0%B0.jpg?alt=media&token=31dbe43a-8a9a-4778-962b-5e1d7ff7b2db&_gl=1*gj6494*_ga*MTkzMTEyODI0NS4xNjk1ODIyNDgx*_ga_CW55HF8NVT*MTY5NjAxMzM0Ny4xMS4xLjE2OTYwMTM2MzkuNjAuMC4w" width="300px" />
                                         Основы выбора инструмента и материалов режущей части
                                     </div>
 
