ae90a07d27c69222d632ba9b9c36bfd9f9c6d132
Destcable <artmobpavlov21@gmail.com>
2023-09-29 20:34:17 +0300

    [UPD]

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -3,7 +3,7 @@ import MainPage from "./pages/main/mainPage";
 import TraningPage from './pages/traning/traningPage';
 import trainings_1 from "./content/tranings/traning_1.json";
 import lecture_1 from "./content/lectures/lecture_1.json";
-import trainings_3 from "./tranings_3.json";
+import lecture_2 from "./content/lectures/lecture_2.json";
 import trainings_4 from "./tranings_4.json";
 import trainings_5 from "./tranings_5.json";
 import quest_1 from "./quest_1.json";
@@ -19,16 +19,16 @@ export const Routes = createBrowserRouter([
         element: <TraningPage traning={lecture_1} />
     },
     {
+        path: "/infoLecture_2",
+        element: <TraningPage traning={lecture_2} />
+    },
+    {
         path: "/traning_1",
         element: <TraningPage traning={trainings_1} />
     },
     {
         path: "/trainingLecture",
         element: <LectureMainPage />
-    },
-    {
-        path: "/infoLecture_2",
-        element: <TraningPage traning={trainings_3} />
     },
     {
         path: "/infoLecture_3",
