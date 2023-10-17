91af56389ee8cfe32afbab59810cfd720fb0f84a
Artem <99603968+Destcable@users.noreply.github.com>
2023-10-17 10:55:20 +0000

    Update highlightAnswers.tsx and traningPage.tsx

--- src/components/AnswersLogic/highlightAnswers.tsx
+++ src/components/AnswersLogic/highlightAnswers.tsx
@@ -1,59 +1,86 @@
-import $ from 'jquery';
+import $ from "jquery";
 import { ANSWER_BUTTON_COLOR } from "../../UI.config";
-import BlockedElement from '../JQuery/blockElements';
-import { UserAnswer } from '../../types/Answer';
+import BlockedElement from "../JQuery/blockElements";
+import { UserAnswer } from "../../types/Answer";
 
-export function highlightAnswersCheckbox(correctAnswers: UserAnswer[] | unknown, values: UserAnswer[] | unknown) {
-    const checkboxes = $("input[name='options']");
+export function highlightAnswersCheckbox(
+  correctAnswers: UserAnswer[] | unknown,
+  values: UserAnswer[] | unknown,
+) {
+  const checkboxes = $("input[name='options']");
 
-    checkboxes.each(function () {
-        const checkboxValue = $(this).val();
-        if (Array.isArray(correctAnswers) && Array.isArray(values)) {
-            if ((correctAnswers.includes(checkboxValue)) || (values && values.includes(checkboxValue))) {
-                const isCorrect = correctAnswers?.includes(checkboxValue);
-                const isIncorrect = values && values.includes(checkboxValue);
-
-                if (isCorrect) {
-                    $(this).parent().css('background-color', ANSWER_BUTTON_COLOR.success);
-                } else if (isIncorrect) {
-                    $(this).parent().css('background-color', ANSWER_BUTTON_COLOR.error);
-                }
-            }
-
-        }
-        BlockedElement($(this));
-    });
-}
-
-export function highlightAnswersSelects(correctAnswers: UserAnswer | unknown, userAnswers: UserAnswer| unknown ) { 
-    const selects = $("select");
-    selects.each(function(index) {
-        const userAnswer = Array.isArray(userAnswers) ? userAnswers[index] : false;
-        const isCorrect = Array.isArray(correctAnswers) ? correctAnswers[index] === userAnswer : false;
-
-        $(this).val(userAnswer);
+  checkboxes.each(function () {
+    const checkboxValue = $(this).val();
+    if (Array.isArray(correctAnswers) && Array.isArray(values)) {
+      if (
+        correctAnswers.includes(checkboxValue) ||
+        (values && values.includes(checkboxValue))
+      ) {
+        const isCorrect = correctAnswers?.includes(checkboxValue);
+        const isIncorrect = values && values.includes(checkboxValue);
 
         if (isCorrect) {
-            $(this).css('background-color', ANSWER_BUTTON_COLOR.success);
-        } else {
-            $(this).css('background-color', ANSWER_BUTTON_COLOR.error);
+          $(this).parent().css("background-color", ANSWER_BUTTON_COLOR.success);
+        } else if (isIncorrect) {
+          $(this).parent().css("background-color", ANSWER_BUTTON_COLOR.error);
         }
+      }
+    }
+    BlockedElement($(this));
+  });
+}
 
-        $(this).val(Array.isArray(userAnswers) ? userAnswers[index] : false);    
+export function highlightAnswersSelects(
+  correctAnswers: UserAnswer | unknown,
+  userAnswers: UserAnswer | unknown,
+) {
+  const selects = $("select");
+  selects.each(function (index) {
+    const userAnswer = Array.isArray(userAnswers) ? userAnswers[index] : false;
+    const isCorrect = Array.isArray(correctAnswers)
+      ? correctAnswers[index] === userAnswer
+      : false;
 
-        BlockedElement($(this));
-    });
+    $(this).val(userAnswer);
+
+    if (isCorrect) {
+      $(this).css("background-color", ANSWER_BUTTON_COLOR.success);
+    } else {
+      $(this).css("background-color", ANSWER_BUTTON_COLOR.error);
+    }
+
+    $(this).val(Array.isArray(userAnswers) ? userAnswers[index] : false);
+
+    BlockedElement($(this));
+  });
+}
+
+export function highlightAnswersText(
+  correctAnswers: UserAnswer | unknown,
+  userAnswers: UserAnswer | unknown,
+) {
+  const textInputs = $('input[type="text"]');
+  textInputs.each((index) => {
+    const userAnswer = Array.isArray(userAnswers) ? userAnswers[index] : false;
+    const isCorrect = Array.isArray(correctAnswers)
+      ? correctAnswers[index] === userAnswer
+      : false;
+  });
 }
 
 export function highlightAnswersRadio(answers: unknown[] | undefined) {
-    const radio = $("label.btn input[type='radio']");
-    BlockedElement(radio);
-    $("input[type='radio']:checked").each(function () {
-        const currentElement = $(this);
-        if (answers?.includes(currentElement.val())) {
-            currentElement.parent().css('background-color', ANSWER_BUTTON_COLOR.success);
-        } else {
-            currentElement.parent().css('background-color', ANSWER_BUTTON_COLOR.error);
-        }
-    })
+  const radio = $("label.btn input[type='radio']");
+  BlockedElement(radio);
+  $("input[type='radio']:checked").each(function () {
+    const currentElement = $(this);
+    if (answers?.includes(currentElement.val())) {
+      currentElement
+        .parent()
+        .css("background-color", ANSWER_BUTTON_COLOR.success);
+    } else {
+      currentElement
+        .parent()
+        .css("background-color", ANSWER_BUTTON_COLOR.error);
+    }
+  });
 }
--- src/pages/traning/traningPage.tsx
+++ src/pages/traning/traningPage.tsx
@@ -1,5 +1,5 @@
 import { CSSProperties, ReactNode, useState } from "react";
-import $ from 'jquery';
+import $ from "jquery";
 import type { TraningData } from "../../types/Traning";
 import Header from "../../components/ui/Header/Header";
 import HomeButton from "../../components/ui/homeButton/homeButton";
@@ -9,8 +9,16 @@ import ArrowLeft from "../../components/ui/arrowLeft/arrowLeft";
 import TemplateLoader from "../../components/TemplateLoader/TemplateLoader";
 import checkSelectsNotEmpty from "../../components/JQuery/checkSelectsNotEmpty";
 import { ANSWER_BUTTON_COLOR } from "../../UI.config";
-import { highlightAnswersCheckbox, highlightAnswersRadio, highlightAnswersSelects } from "../../components/AnswersLogic/highlightAnswers";
-import { getUserAnswers, saveUserAnswers } from "../../Controllers/answers/saveAnswer";
+import {
+  highlightAnswersCheckbox,
+  highlightAnswersRadio,
+  highlightAnswersSelects,
+  highlightAnswersText,
+} from "../../components/AnswersLogic/highlightAnswers";
+import {
+  getUserAnswers,
+  saveUserAnswers,
+} from "../../Controllers/answers/saveAnswer";
 import createAnswerButton from "../../components/JQuery/AnswerButton/createAnswerButton";
 import { removeAnswerButton } from "../../components/JQuery/AnswerButton/removeAnswerButton";
 import getSelects from "../../components/JQuery/getSelects";
@@ -19,223 +27,251 @@ import getInputTexts from "../../components/JQuery/getInputTexts";
 import getRadio from "../../components/JQuery/getRadio";
 
 interface TraningPageProps {
-    traning: TraningData[],
-    child?: ReactNode
+  traning: TraningData[];
+  child?: ReactNode;
 }
 
 const arrows: CSSProperties = {
-    bottom: "20px",
-    right: "50px",
-}
+  bottom: "20px",
+  right: "50px",
+};
 
 const TraningPage = (props: TraningPageProps) => {
-    const [countActiveTab, setActiveTab] = useState(0);
-    const [isEnabledButton, setEnabledButton] = useState(false);
-    const currentAnswers = getUserAnswers()[countActiveTab + 1];
+  const [countActiveTab, setActiveTab] = useState(0);
+  const [isEnabledButton, setEnabledButton] = useState(false);
+  const currentAnswers = getUserAnswers()[countActiveTab + 1];
 
-    const traningType = props.traning[countActiveTab].type;
+  const traningType = props.traning[countActiveTab].type;
 
-    function handleSelectChange() {
-        if (checkSelectsNotEmpty()) {
-            if ($('#send-answers__button').length === 0) { 
-                createAnswerButton();
-            }
-            answerButtonClick();
-        }
+  function handleSelectChange() {
+    if (checkSelectsNotEmpty()) {
+      if ($("#send-answers__button").length === 0) {
+        createAnswerButton();
+      }
+      answerButtonClick();
+    }
+  }
+
+  function addCountTab() {
+    setEnabledButton(false);
+
+    if (getUserAnswers()[countActiveTab + 2]) {
+      setEnabledButton(true);
     }
 
-    function addCountTab() {
-        setEnabledButton(false);
-        
-        if (getUserAnswers()[countActiveTab + 2]) {
-            setEnabledButton(true);
-        }
+    setCountActiveTab(countActiveTab + 1);
+  }
 
-        setCountActiveTab(countActiveTab + 1);
+  function takeCountTab() {
+    setEnabledButton(false);
+
+    if (getUserAnswers()[countActiveTab]) {
+      setEnabledButton(true);
     }
 
-    function takeCountTab() {
-        setEnabledButton(false);
-        
-        if (getUserAnswers()[countActiveTab]) {
-            setEnabledButton(true);
-        }
-        
-        setCountActiveTab(countActiveTab - 1);
+    setCountActiveTab(countActiveTab - 1);
+  }
+
+  const tabs = [];
+
+  const HTMLContent = TemplateLoader(props.traning[countActiveTab].component);
+
+  const ImageUrl = props.traning[countActiveTab].image
+    ? props.traning[countActiveTab].image
+    : undefined;
+
+  function setCountActiveTab(count: number) {
+    if (count < props.traning.length && count >= 0) {
+      return setActiveTab(count);
     }
 
-    const tabs = [];
+    return console.error("Данный таб отсутствует");
+  }
 
-    const HTMLContent = TemplateLoader(props.traning[countActiveTab].component);
+  for (let index = 0; index < props.traning.length; index++) {
+    tabs.push(<div key={index} className="tab"></div>);
+  }
 
-    const ImageUrl = props.traning[countActiveTab].image ? props.traning[countActiveTab].image : undefined;
+  $('input[name="options"]')
+    .off("change")
+    .on("change", function () {
+      const selectedElements = $('input[name="options"]:checked');
 
-    function setCountActiveTab(count: number) {
-        if (count < props.traning.length && count >= 0) {
-            return setActiveTab(count);
+      if (selectedElements.length > 0) {
+        if ($("#send-answers__button").length === 0) {
+          createAnswerButton();
+          answerButtonClick();
         }
+      }
 
-        return console.error('Данный таб отсутствует');
-    }
+      if (selectedElements.length === 0) {
+        removeAnswerButton();
+      }
 
-    for (let index = 0; index < props.traning.length; index++) {
-        tabs.push(<div key={index} className="tab"></div>);
-    }
-
-    $('input[name="options"]').off('change').on('change', function () {
-        const selectedElements = $('input[name="options"]:checked');
-
-        if (selectedElements.length > 0) {
-            if ($('#send-answers__button').length === 0) {
-                createAnswerButton();
-                answerButtonClick();
-            }
+      if (traningType === "checkbox") {
+        if ($(this).is(":checked")) {
+          $(this)
+            .parent()
+            .css("background-color", ANSWER_BUTTON_COLOR.selected);
+        } else {
+          $(this).parent().css("background-color", "");
         }
+      }
+    });
 
-        if (selectedElements.length === 0) {
-            removeAnswerButton();
+  if (traningType === "radio") {
+    $('input[type="radio"]').on("change", function () {
+      $('input[type="radio"]').parent().css("background-color", "");
+
+      if ($(this).is(":checked")) {
+        $(this).parent().css("background-color", ANSWER_BUTTON_COLOR.selected);
+      }
+    });
+  }
+
+  if (traningType === "text") {
+    const textInputs = $('input[type="text"]');
+
+    textInputs.on("input", function () {
+      let allFilled = true;
+
+      textInputs.each(function () {
+        const inputValue = $(this).val();
+        if (typeof inputValue === "string" && inputValue.trim() === "") {
+          allFilled = false;
+          return false;
+        }
+      });
+
+      if (allFilled) {
+        if ($("#send-answers__button").length === 0) {
+          createAnswerButton();
+          answerButtonClick();
+        }
+      } else {
+        removeAnswerButton();
+      }
+    });
+  }
+
+  if (traningType === "select") {
+    currentAnswers
+      ? highlightAnswersSelects(
+          props.traning[countActiveTab].answers,
+          currentAnswers,
+        )
+      : $("select")
+          .off("change")
+          .on("change", () => handleSelectChange());
+  }
+
+  if (traningType === "checkbox" && currentAnswers) {
+    highlightAnswersCheckbox(
+      props.traning[countActiveTab].answers,
+      currentAnswers,
+    );
+  }
+
+  function answerButtonClick() {
+    $("#send-answers__button")
+      .off("click")
+      .on("click", function () {
+        removeAnswerButton();
+        setEnabledButton(true);
+
+        if (traningType === "select") {
+          saveUserAnswers(countActiveTab + 1, getSelects());
         }
 
         if (traningType === "checkbox") {
-            if ($(this).is(':checked')) {
-                $(this).parent().css('background-color', ANSWER_BUTTON_COLOR.selected);
-            } else {
-                $(this).parent().css('background-color', '');
-            }
+          saveUserAnswers(countActiveTab + 1, getCheckboxes());
+          highlightAnswersCheckbox(
+            props.traning[countActiveTab].answers,
+            currentAnswers,
+          );
         }
-    });
 
-    if (traningType === "radio") {
-        $('input[type="radio"]').on("change", function () {
+        if (traningType === "text") {
+          saveUserAnswers(countActiveTab + 1, getInputTexts());
+          highlightAnswersText(
+            props.traning[countActiveTab].answers,
+            currentAnswers,
+          );
+        }
 
-            $('input[type="radio"]').parent().css('background-color', '');
+        if (traningType === "radio") {
+          saveUserAnswers(countActiveTab + 1, getRadio());
+          highlightAnswersRadio(props.traning[countActiveTab].answers);
+        }
+      });
+  }
 
-            if ($(this).is(':checked')) {
-                $(this).parent().css('background-color', ANSWER_BUTTON_COLOR.selected);
-            }
-        });
-    }
+  return (
+    <>
+      <Header />
 
-    if (traningType === "text") { 
-        const textInputs = $('input[type="text"]');
+      <div className="bg_color_block d-flex flex-column ">
+        <div className="d-flex justify-content-center align-center">
+          <div className="content1 d-flex flex-column max-content w-100">
+            <HomeButton href="/" />
 
-        textInputs.on('input', function() {
-            let allFilled = true;
-
-            textInputs.each(function() {
-                const inputValue = $(this).val();
-                if (typeof inputValue === 'string' && inputValue.trim() === '') {
-                    allFilled = false;
-                    return false; 
-                }
-            });
-    
-            if (allFilled) {
-                if ($('#send-answers__button').length === 0) {
-                    createAnswerButton();
-                    answerButtonClick();
-                }
-            } else {
-                removeAnswerButton();
-            }
-        });
-    }
-
-    if (traningType === 'select') {
-        currentAnswers ? highlightAnswersSelects(props.traning[countActiveTab].answers, currentAnswers) : $('select').off('change').on('change', () => handleSelectChange());
-    }
-
-    if (traningType === 'checkbox' && currentAnswers) {
-        highlightAnswersCheckbox(props.traning[countActiveTab].answers, currentAnswers);
-    }
-
-    function answerButtonClick() {
-        $('#send-answers__button').off('click').on('click', function () {
-            removeAnswerButton();
-            setEnabledButton(true);
-            
-            if (traningType === "select") {
-                saveUserAnswers(countActiveTab + 1, getSelects());
-            }
-
-            if (traningType === "checkbox") {
-                saveUserAnswers(countActiveTab + 1, getCheckboxes());
-                highlightAnswersCheckbox(props.traning[countActiveTab].answers, currentAnswers);
-            }
-
-            if (traningType === "text") {
-                saveUserAnswers(countActiveTab + 1, getInputTexts());
-            }
-
-            if (traningType === "radio") {
-                saveUserAnswers(countActiveTab + 1, getRadio());
-                highlightAnswersRadio(props.traning[countActiveTab].answers);
-            }
-
-        });
-    }
-
-    return (
-        <>
-            <Header />
-
-            <div className="bg_color_block d-flex flex-column ">
-
-
-                <div className="d-flex justify-content-center align-center">
-                    <div className="content1 d-flex flex-column max-content w-100">
-                        <HomeButton
-                            href="/"
-                        />
-
-                        <div className="container1">
-                            <div className="container_header">
-                                <div className="d-flex align-items-center justify-content-between">
-                                    <p className="container_title">Динамическая инфографика</p>
-                                    <p className="text-white pe-3">{`${countActiveTab + 1} / ` + props.traning.length}</p>
-                                </div>
-                                <div className="d-flex tab-exercise-container">
-                                    {tabs}
-                                    {/* <div className="active_tab"></div> */}
-                                </div>
-                            </div>
-                            <div className="container-exercise">
-                                <div className="d-flex justify-content-between">
-                                    <p className="text-zadanie">{props.traning[countActiveTab].title}</p>
-                                </div>
-
-                                <div className="d-flex container-filling align-center justify-content-center">
-
-                                    {ImageUrl &&
-                                        <div className="img-exercise">
-                                            <img src={ImageUrl} className="img-exercise1" style={{ maxHeight: "400px" }} />
-                                        </div>
-                                    }
-                                    {props.traning[countActiveTab].component &&
-                                        <div>
-                                            <div dangerouslySetInnerHTML={{ __html: HTMLContent }} />
-                                        </div>
-                                    }
-
-                                </div>
-
-                                <div id="arrows" className="d-flex w-100 justify-content-end " style={arrows}>
-                                    {isEnabledButton && countActiveTab > 0 && <ArrowLeft onClick={takeCountTab} />}
-                                    {isEnabledButton && <ArrowRight id="" onClick={addCountTab} />}
-                                </div>
-
-                            </div>
-                        </div>
-
-                    </div>
-
+            <div className="container1">
+              <div className="container_header">
+                <div className="d-flex align-items-center justify-content-between">
+                  <p className="container_title">Динамическая инфографика</p>
+                  <p className="text-white pe-3">
+                    {`${countActiveTab + 1} / ` + props.traning.length}
+                  </p>
                 </div>
+                <div className="d-flex tab-exercise-container">
+                  {tabs}
+                  {/* <div className="active_tab"></div> */}
+                </div>
+              </div>
+              <div className="container-exercise">
+                <div className="d-flex justify-content-between">
+                  <p className="text-zadanie">
+                    {props.traning[countActiveTab].title}
+                  </p>
+                </div>
+
+                <div className="d-flex container-filling align-center justify-content-center">
+                  {ImageUrl && (
+                    <div className="img-exercise">
+                      <img
+                        src={ImageUrl}
+                        className="img-exercise1"
+                        style={{ maxHeight: "400px" }}
+                      />
+                    </div>
+                  )}
+                  {props.traning[countActiveTab].component && (
+                    <div>
+                      <div dangerouslySetInnerHTML={{ __html: HTMLContent }} />
+                    </div>
+                  )}
+                </div>
+
+                <div
+                  id="arrows"
+                  className="d-flex w-100 justify-content-end "
+                  style={arrows}
+                >
+                  {isEnabledButton && countActiveTab > 0 && (
+                    <ArrowLeft onClick={takeCountTab} />
+                  )}
+                  {isEnabledButton && (
+                    <ArrowRight id="" onClick={addCountTab} />
+                  )}
+                </div>
+              </div>
             </div>
-            <Tringle
-                background="CFDEEE"
-            />
-        </>
-    )
-}
+          </div>
+        </div>
+      </div>
+      <Tringle background="CFDEEE" />
+    </>
+  );
+};
 
 export default TraningPage;
