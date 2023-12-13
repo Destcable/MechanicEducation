84dfc014607dfc3ad794206cae9440a259cc209a
Artem <99603968+Destcable@users.noreply.github.com>
2023-12-13 20:16:34 +0300

    Delete COK_2_SODERZHATELNOE_OPISANIE directory

--- COK_2_SODERZHATELNOE_OPISANIE/src/components/JQuery/blockElements.ts
+++
@@ -1,6 +0,0 @@
-import $ from 'jquery';
-
-export default function BlockedElement(HTMLelement: JQuery<HTMLElement>) { 
-    $(HTMLelement).prop("disabled", true);
-    $(HTMLelement).parent().addClass("disabled");
-}
