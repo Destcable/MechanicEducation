73c7203567bceb9fcd675784242ae914e116f8d1
Destcable <artmobpavlov21@gmail.com>
2024-03-15 01:30:15 +0300

    [upd]

--- [CMD]/install.bat
+++ [CMD]/install.bat
@@ -1,7 +1,5 @@
 @echo off
 
-start /B cmd /c "cd .. & docker-compose up --build"
 start /B cmd /c "cd ../server & npm install"
 start /B cmd /c "cd ../admin & npm install"
 start /B cmd /c "cd ../client & npm install"
-start /B cmd /c "cd ../server & npm run dbpush"
--- [CMD]/start.bat
+++ [CMD]/start.bat
@@ -1,6 +1,7 @@
 @echo off
 
 start /B cmd /c "docker-compose up --build"
-start /B cmd /c "cd ./server & npm run dev"
-start /B cmd /c "cd ./admin & npm run dev"
-start /B cmd /c "cd ./client & npm run dev"
+start /B cmd /c "cd ../server & npm run dbpush"
+start /B cmd /c "cd ../server & npm run dev"
+start /B cmd /c "cd ../admin & npm run dev"
+start /B cmd /c "cd ../client & npm run dev"
