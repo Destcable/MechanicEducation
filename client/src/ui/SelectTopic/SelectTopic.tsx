6e525da58a9e4f4b2e0fd066024776c89f4ef94e
Destcable <artmobpavlov21@gmail.com>
2024-04-05 23:47:46 +0300

    [upd]

--- client/src/ui/SelectTopic/SelectTopic.tsx
+++ client/src/ui/SelectTopic/SelectTopic.tsx
@@ -3,28 +3,31 @@ import Tringle from "../../components/ui/tringle/tringle";
 import { Topic } from "../../interfaces/topic";
 import Button from "../Button/Button";
 
-interface ISelectThemeProps { 
+interface ISelectThemeProps {
     topics: Topic[],
     onClick: (data: Topic) => void
 }
 
-const SelectTopic = (props: ISelectThemeProps) => { 
-    return( 
+const SelectTopic = (props: ISelectThemeProps) => {
+    return (
         <>
-        <div className="d-grid m-5 gap-4">
-            {props.topics.map((item, key) => 
-                <Button 
-                    key={key}
-                    id={item.id}
-                    className='text-white w-100 p-2'
-                    onClick={() => props.onClick(item)}
-                >
-                    {item.name}
-                </Button> 
-            )}
-        </div>
-        <Tringle />
-        <Footer />
+            <div className="d-grid m-5 gap-4">
+                {props.topics.map((item, key) =>
+                    <Button
+                        key={key}
+                        id={item.id}
+                        className='text-white w-100 p-2'
+                        onClick={() => props.onClick(item)}
+                    >
+                        {item.name}
+                    </Button>
+                )}
+            </div>
+
+            <div style={{ position: 'fixed', bottom: 0, left: 0, width: '100%', boxSizing: 'border-box', zIndex: 999 }}>
+                <Tringle />
+                <Footer />
+            </div>
         </>
     )
 };
