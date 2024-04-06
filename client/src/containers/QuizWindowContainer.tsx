7b4c5a5b9a25ef71220714e42a2bf8497b1c0c2a
Destcable <artmobpavlov21@gmail.com>
2024-04-06 12:26:19 +0300

    [upd]

--- client/src/containers/QuizWindowContainer.tsx
+++ client/src/containers/QuizWindowContainer.tsx
@@ -1,21 +1,50 @@
-import QuizWindow from "../ui/QuizWindow/QuizWindow";
+import React, { useState } from 'react';
+import QuizWindow from '../ui/QuizWindow/QuizWindow';
+import { ANSWER_BUTTON_COLOR } from '../UI.config';
 
 interface IQuizWindowContainer {
-    dataTask: any,
-};
+    dataTask: {
+        title: string;
+        dataQuiz: {
+            title: string;
+        }[];
+    };
+}
 
-const QuizWindowContainer = (props: IQuizWindowContainer) => {
-    const TaskTitle = props.dataTask.title;
-    const TaskContent = props.dataTask.dataQuiz;
+const QuizWindowContainer: React.FC<IQuizWindowContainer> = ({ dataTask }) => {
+    const { title: TaskTitle, dataQuiz: TaskContent } = dataTask;
+    const [selectedAnswers, setSelectedAnswers] = useState<string[]>([]);
 
-    return <QuizWindow title={TaskTitle}>
-        {TaskContent && TaskContent.map((task: any) =>
-            <label className="btn colorButton d-block mb-2 w-100" style={{ backgroundColor: "#A2BEDD" }}>
-                <input type="checkbox" name="options" value="A" autoComplete="off" />{task.title}
-            </label>
-        )}
+    const handleOptionChange = (task: string) => {
+        setSelectedAnswers(prevState =>
+            prevState.includes(task) ? prevState.filter(answer => answer !== task) : [...prevState, task]
+        );
+    };
 
-    </QuizWindow>
+    return (
+        <QuizWindow title={TaskTitle}>
+            {TaskContent &&
+                TaskContent.map((task, idx) => (
+                    <label
+                        key={idx}
+                        className="btn colorButton d-block mb-2 w-100"
+                        style={{
+                            backgroundColor: selectedAnswers.includes(task.title)
+                                ? ANSWER_BUTTON_COLOR.selected
+                                : ANSWER_BUTTON_COLOR.default,
+                        }}
+                    >
+                        <input
+                            type="checkbox"
+                            name="options"
+                            style={{ display: 'none' }}
+                            onChange={() => handleOptionChange(task.title)}
+                        />
+                        {task.title}
+                    </label>
+                ))}
+        </QuizWindow>
+    );
 };
 
 export default QuizWindowContainer;
