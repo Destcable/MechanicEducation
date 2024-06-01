2994eff83f34f25bfee645161f61361a0a565597
Destcable <artmobpavlov21@gmail.com>
2024-06-02 00:49:48 +0300

    [upd]

--- client/src/ui/SelectTopic/SelectTopic.tsx
+++ client/src/ui/SelectTopic/SelectTopic.tsx
@@ -15,8 +15,20 @@ const SelectTopic = (props: ISelectThemeProps) => {
     return (
         <>
             <Header />
-            <div className=" d-flex h-100">
-                <h1 className="h-100 bg-light">123</h1>
+            <div className="d-flex h-100">
+                <div className="d-flex h-100 p-3 w-25" style={{ backgroundColor: '#4276af6b' }}>
+                    <h6>Выбор темы:</h6>
+                    <br />
+                    <br />
+                    <span>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. 
+                        Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века. 
+                        В то время некий безымянный печатник создал большую коллекцию размеров и форм шрифтов, 
+                        используя Lorem Ipsum для распечатки образцов. Lorem Ipsum не только успешно пережил 
+                        без заметных изменений пять веков, но и перешагнул в электронный дизайн. 
+                        Его популяризации в новое время послужили публикация листов 
+                        Letraset с образцами Lorem Ipsum в 60-х годах и, в более недавнее время, 
+                        программы электронной вёрстки типа Aldus PageMaker, в шаблонах которых используется Lorem Ipsum.</span>
+                </div>
                 <div className="w-100 m-5 gap-4">
                     {props.topics.map((item, key) =>
                         <Button
