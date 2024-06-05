eace2f3403395db0fd5b01890e9ac9e344729d62
Destcable <artmobpavlov21@gmail.com>
2024-06-05 14:37:29 +0300

    [upd]

--- client/src/containers/LectureWindowContainer.tsx
+++ client/src/containers/LectureWindowContainer.tsx
@@ -1,17 +1,22 @@
 import { get } from 'lodash';
 import LectureWindow from "../ui/LectureWindow/LectureWindow";
 import NextButton from '../ui/NextButton/NextButton';
+import { useQueryListThemeTasks } from '../hooks/useQueryThemeTasks';
 
 interface ILectureWindowContainerProps {
     dataTask: any,
 };
 
 const LectureWindowContainer = ({ dataTask }: ILectureWindowContainerProps) => {
+    const { data } = useQueryListThemeTasks(
+        get(dataTask, 'themeId', '')
+    );
     const TaskTitle = get(dataTask, 'title', '');
     const TaskContent = get(dataTask, 'dataLecture', '');
 
-    const nextTask = () => {
-        console.log(dataTask);
+    const nextTask = () => {    
+
+        console.log(data);
     };
 
     return dataTask && <LectureWindow title={TaskTitle} nextButton={<NextButton onClick={() => nextTask()} />}>
