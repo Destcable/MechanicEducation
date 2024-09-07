33009555909ac2a2a1ad04c8da2594ca2cd5e4e9
Destcable <artmobpavlov21@gmail.com>
2024-09-07 22:26:54 +0300

    [upd] add auth service

---
+++ services/auth-service/src/controllers/authController.js
@@ -0,0 +1,26 @@
+import { prisma } from "../config/prisma.js"
+import { validate } from "../utils/validate.js";
+
+export const authController = { 
+    login: async (req, res) => {
+        const validateData = validate(req, { 
+            login: 'required',
+            password: 'required'
+        })
+
+        if (validateData.errors) { 
+            return res.status(404).send(validateData.errors)
+        }
+        const { login, password } = validateData;
+
+        const user = await prisma.user.findUnique({ 
+            where: { login, password }     
+        })
+
+        return res.send( user ? {status: true} : {status: false} )
+    },
+
+    logout: () => {
+
+    }
+}
