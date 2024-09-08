3eaa1805ffded49fc3bd3b36c7f3dee9e1f0b9fd
Destcable <artmobpavlov21@gmail.com>
2024-09-08 16:55:11 +0300

    [upd] refactoring TeacherPage

--- client/src/core/config/routes.config.ts
+++ client/src/core/config/routes.config.ts
@@ -3,3 +3,7 @@ import { SERVER_URI } from "./api.config";
 export const authAPIRoutes = { 
     login: () => `${SERVER_URI}/auth/login`
 }
+
+export const userAPIRoutes = { 
+    getAllTeachers: () => `${SERVER_URI}/users/teachers`
+}
--- client/src/pages/Teachers/TeachersPage.tsx
+++ client/src/pages/Teachers/TeachersPage.tsx
@@ -1,9 +1,45 @@
+import axios from "axios";
 import { TeacherCard } from "../../ui/TeacherCard/TeacherCard";
+import { userAPIRoutes } from "../../core/config/routes.config";
+import { useEffect, useState } from "react";
+import Loading from "../../ui/Loading";
+
+interface Teacher { 
+    id: number;
+    name: string;
+}
 
 const TeachersPage = () => {
+    const [teachers, setTeachers] = useState<Teacher[] | null>(null);
+    const [loading, setLoading] = useState<boolean>(false);
+
+    useEffect(() => {
+        const fetchTeachers = async () => {
+            setLoading(true);
+
+            try {
+                const { data } = await axios.get(userAPIRoutes.getAllTeachers());
+                setTeachers(data);
+            } finally {
+                setLoading(false)
+            }
+        };
+
+        fetchTeachers();
+    }, []);
+
+    if (loading) {
+        return <Loading />
+    }
+
     return <div className="d-flex gap-3">
-        <TeacherCard name="Иванов Иван" jobTitle="Учитель математики" />
-        <TeacherCard name="Павлов Артем" jobTitle="Учитель информатики" />
+        {teachers && teachers.map(teacher => (
+            <TeacherCard
+                key={teacher.id}
+                name={teacher.name}
+                jobTitle="Учитель математики"
+            />
+        ))}
     </div>
 };
 
